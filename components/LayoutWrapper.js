4547ce785e25a92fb76f88116cb220282dda56f3
Adrien Lacquemant <github@alaq.io>
2021-08-21 08:59:19 -0400

    Change site metadata

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -13,11 +13,11 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex flex-col justify-between h-screen">
         <header className="flex items-center justify-between py-10">
           <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
+            <Link href="/" aria-label="adrien.sh">
               <div className="flex items-center justify-between">
-                <div className="mr-3">
+                {/* <div className="mr-3">
                   <Logo />
-                </div>
+                </div> */}
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
