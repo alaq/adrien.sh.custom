c70713a6d126e4240f3c96727b213f68e8e100d8
Adrien Lacquemant <github@alaq.io>
2021-08-24 23:32:27 -0400

    Simplify article list

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,10 @@ import { PageSEO } from '@/components/SEO'
 // import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
-import formatDate from '@/lib/utils/formatDate'
+import { formatDateWithoutYear } from '@/lib/utils/formatDate'
 
 const MAX_DISPLAY = 5
+let year = 0
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('articles')
@@ -52,24 +53,39 @@ export default function Home({ posts }) {
             .
           </p>
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+        <ul>
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
+            const { slug, date, title } = frontMatter
+            const currentYear = new Date(frontMatter.date).getFullYear()
+            let printYear
+
+            if (currentYear !== year) {
+              printYear = true
+              year = currentYear
+            } else {
+              printYear = false
+            }
+
             return (
-              <li key={slug} className="py-12">
+              <li key={slug} className="py-4">
+                {printYear ? (
+                  <h2 className="text-xl font-bold leading-8 tracking-tight py-4">{year}</h2>
+                ) : (
+                  <></>
+                )}
                 <article>
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
                     <dl>
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date)}</time>
+                        <time dateTime={date}>{formatDateWithoutYear(date)}</time>
                       </dd>
                     </dl>
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                          <h2 className="text-xl font-bold leading-8 tracking-tight">
                             <Link
                               href={`/articles/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
@@ -77,24 +93,7 @@ export default function Home({ posts }) {
                               {title}
                             </Link>
                           </h2>
-                          {/* <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div> */}
                         </div>
-                        <div className="prose text-gray-500 max-w-none dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/articles/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
                       </div>
                     </div>
                   </div>
